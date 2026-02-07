6f7789be96e0db663795c185dc56e54d13633869
hpr <habs@sdf.org>
2026-02-06 20:21:24 -0500

    in-memory thumbnail

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -37,6 +37,7 @@ export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
 }
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
+  const MAX_UPLOAD_SIZE = 10 << 20; // 10 MB
   const { videoId } = req.params as { videoId?: string };
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
@@ -47,7 +48,14 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const thumbnail = (await req.formData()).get("thumbnail");
+  if (!(thumbnail instanceof File)) throw new BadRequestError("bad thumbnail type");
+  if (thumbnail.size > MAX_UPLOAD_SIZE) throw new BadRequestError("bad thumbnail size");
+  const video = getVideo(cfg.db, videoId);
+  if (userID !== video?.userID) throw new UserForbiddenError("bad user");
+  videoThumbnails.set(video.id, { data: await thumbnail.arrayBuffer(), mediaType: thumbnail.type });
+  video.thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${video.id}`;
+  updateVideo(cfg.db, video);
 
-  return respondWithJSON(200, null);
+  return respondWithJSON(200, video);
 }
